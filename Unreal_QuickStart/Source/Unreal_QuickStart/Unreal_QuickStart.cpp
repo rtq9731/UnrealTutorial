a56c526950aec4ede535becaa710fcce33c29b2b
COCONUT <rtq9731@gmail.com>
2022-10-16 11:57:34 +0900

    실수로 블루프린트 프로젝트 생성해서 삭제

--- Unreal_QuickStart/Source/Unreal_QuickStart/Unreal_QuickStart.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Unreal_QuickStart.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Unreal_QuickStart, "Unreal_QuickStart" );
