dc8bef0da05ac24a97644e2ada122a291e6b5caf
COCONUT <rnjstjdqls9731@gmail.com>
2022-10-18 19:08:09 +0900

    Pawn 관련 튜토리얼 프로젝트 생성

---
+++ PlayerPawnTuto/Source/PlayerPawnTuto/PlayerPawnTutoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PlayerPawnTutoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLAYERPAWNTUTO_API APlayerPawnTutoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
