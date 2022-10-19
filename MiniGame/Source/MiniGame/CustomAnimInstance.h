b41e64644f8ef19740929a0dd07376b48e4800a9
COCONUT <rnjstjdqls9731@gmail.com>
2022-10-19 11:58:43 +0900

    미니게임 프로젝트 생성

---
+++ MiniGame/Source/MiniGame/CustomAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "CustomAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MINIGAME_API UCustomAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
